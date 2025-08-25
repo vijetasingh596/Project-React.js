7af2e2ca02aa240bf0816fa3863f1878f616d934
vijetasingh596 <kunal11152004@gmail.com>
2025-08-25 22:14:38 +0530

    update all

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,38 +1,43 @@
-import React, { useEffect, useState } from 'react'
-import NavBar from '../components/NavBar'
-import { AxiosInstance } from '../routes/axiosInstance'
+import React, { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
+import { AxiosInstance } from "../routes/axiosInstance";
 
 const Home = () => {
-    const [allBlogs,setAllBlogs]=useState([])
-    async function getAllBlogs() {
-        let res=await AxiosInstance.get("/blogs")
-        console.log(res.data);
-        setAllBlogs(res.data)  
-    }
-    useEffect(()=>{
-        getAllBlogs()
-    },[])
+  const [allBlogs, setAllBlogs] = useState([]);
+  async function getAllBlogs() {
+    let res = await AxiosInstance.get("/blogs");
+    console.log(res.data);
+    setAllBlogs(res.data);
+  }
+  useEffect(() => {
+    getAllBlogs();
+  }, []);
   return (
     <div>
-        <NavBar/>
-        <section>
-            {
-           allBlogs.length===0?(<h2>NO blogs available</h2>):(<article>
-           {
-            allBlogs.map((blog)=>{
-                let {id,title,description,category}=blog
-                return <div key={id}>
-                 <h4>{category}</h4>
-                 <h1>{title}</h1>
-                 <p>{description}</p>
+      <NavBar />
+      <section className="h-full w-full">
+        {allBlogs.length === 0 ? (
+          <h2>NO blogs available</h2>
+        ) : (
+          <article className=" flex justify-evenly gap-5 flex-wrap">
+            {allBlogs.map((blog) => {
+              let { id, title, description, category } = blog;
+              return (
+                <div
+                  key={id}
+                  className="h-[200px] flex shadow-2xl  flex-col justify-center w-[300px] rounded-2xl"
+                >
+                  <h4>{category}</h4>
+                  <h1>{title}</h1>
+                  <p>{description}</p>
                 </div>
-            })
-           }
-           </article>)
-        }
-        </section>
+              );
+            })}
+          </article>
+        )}
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
